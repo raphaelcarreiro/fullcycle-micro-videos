25c88acba56b0f4d9c342afc15a3acad2471628d
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2022-06-16 10:44:50 -0300

    Adiciona nova Exception

--- src/@seedwork/errors/validation-error.ts
+++ src/@seedwork/errors/validation-error.ts
@@ -1,1 +1,10 @@
+import { FieldErrors } from "@seedwork/domain/validators/validator-fields.interface";
+
 export class ValidationError extends Error {}
+
+export class EntityValidationError extends Error {
+  constructor(public error: FieldErrors) {
+    super("Entity validation Error");
+    this.name = "EntityValidationError";
+  }
+}
