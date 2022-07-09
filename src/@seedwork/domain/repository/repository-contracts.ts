7e6b2ce016d624b552bc1e2e41cd607259a04352
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2022-07-09 14:34:12 -0300

    Adiciona repository-contracts.ts

--- src/@seedwork/domain/repository/repository-contracts.ts
+++ src/@seedwork/domain/repository/repository-contracts.ts
@@ -94,6 +94,56 @@ export class SearchParams {
   }
 }
 
-export interface SearchableRepositoryInterface<E extends Entity, Input, Output> extends RepositoryInterface<E> {
+type SearchResultProps<E extends Entity, Filter> = {
+  items: E[];
+  total: number;
+  current_page: number;
+  per_page: number;
+  sort: string | null;
+  sort_direction: string | null;
+  filter: string | null;
+};
+export class SearchResult<E extends Entity, Filter = string> {
+  readonly items: E[];
+  readonly total: number;
+  readonly current_page: number;
+  readonly per_page: number;
+  readonly last_page: number;
+  readonly sort: string | null;
+  readonly sort_direction: string | null;
+  readonly filter: string | null;
+
+  constructor(props: SearchResultProps<E, Filter>) {
+    this.items = props.items;
+    this.total = props.total;
+    this.current_page = props.current_page;
+    this.per_page = props.per_page;
+    this.last_page = Math.ceil(props.total / props.per_page);
+    this.sort = props.sort;
+    this.sort_direction = props.sort_direction;
+    this.filter = props.filter;
+  }
+
+  toJSON() {
+    return {
+      items: this.items,
+      total: this.total,
+      current_page: this.current_page,
+      per_page: this.per_page,
+      last_page: this.last_page,
+      sort: this.sort,
+      sort_direction: this.sort_direction,
+      filter: this.filter,
+    };
+  }
+}
+
+export interface SearchableRepositoryInterface<
+  E extends Entity,
+  Filter = string,
+  Input = SearchParams,
+  Output = SearchResult<E, Filter>
+> extends RepositoryInterface<E> {
+  sortableFields: string[];
   search(props: Input): Promise<Output>;
 }
