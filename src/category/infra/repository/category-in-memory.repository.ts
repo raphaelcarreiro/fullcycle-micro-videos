26ce825e33ab2649d116166aace00876169d54a1
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2022-08-06 12:36:14 -0300

    Implementa métodos applyFilter e applySort para CategoryInMemoryRepository

--- src/category/infra/repository/category-in-memory.repository.ts
+++ src/category/infra/repository/category-in-memory.repository.ts
@@ -1,7 +1,31 @@
-import { SearchableRepositoryInterface } from "@seedwork/domain/repository/repository-contracts";
+import { SortDirection } from "@seedwork/domain/repository/repository-contracts";
 import { Category } from "category/domain/entities/category";
+import CategoryRepository from "category/domain/repository/category.repository.interface";
 import { InMemorySearchableRepository } from "../../../@seedwork/domain/repository/in-memory.repository";
 
-class CategoryInMemoryRepository
+export class CategoryInMemoryRepository
   extends InMemorySearchableRepository<Category>
-  implements SearchableRepositoryInterface<Category, any, any> {}
+  implements CategoryRepository.RepositoryInterface
+{
+  sortableFields: string[] = ["created_at", "name"];
+
+  protected async applyFilter(items: Category[], filter: CategoryRepository.Filter): Promise<Category[]> {
+    if (!filter) {
+      return items;
+    }
+
+    return items.filter(item => item.props.name.toLowerCase().includes(filter.toLowerCase()));
+  }
+
+  protected async applySort(
+    items: Category[],
+    sort?: string | null,
+    sort_direction?: SortDirection | null
+  ): Promise<Category[]> {
+    if (!sort) {
+      return super.applySort(items, "created_at", sort_direction ?? "desc");
+    }
+
+    return super.applySort(items, sort, sort_direction ?? "desc");
+  }
+}
