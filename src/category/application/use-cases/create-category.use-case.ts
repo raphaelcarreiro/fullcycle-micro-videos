39cbd2c2f4f4fdf11f65f8f320e8e3afacbc0a8e
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2022-09-03 13:51:31 -0300

    adiciona testes

---
+++ src/category/application/use-cases/create-category.use-case.ts
@@ -0,0 +1,24 @@
+import { Category } from "../../../category/domain/entities/category";
+import CategoryRepository from "category/domain/repository/category.repository.interface";
+import { CategoryOutput, CategoryOutputMapper } from "../dto/category-output";
+import { UseCase } from "@seedwork/application/use-case";
+
+export default class CreateCategoryUseCase implements UseCase<Input, Output> {
+  constructor(private readonly repository: CategoryRepository.RepositoryInterface) {}
+
+  async execute(input: Input): Promise<Output> {
+    const entity = new Category(input);
+
+    this.repository.insert(entity);
+
+    return CategoryOutputMapper.toOutput(entity);
+  }
+}
+
+export type Input = {
+  name: string;
+  description?: string;
+  is_active?: boolean;
+};
+
+export type Output = CategoryOutput;
