7e23d2a8c70d540163aeb362dc8e8e54c43bb75c
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2022-09-03 13:51:22 -0300

    atualiza SearchResult

--- src/@seedwork/domain/repository/repository-contracts.ts
+++ src/@seedwork/domain/repository/repository-contracts.ts
@@ -103,7 +103,7 @@ type SearchResultProps<E extends Entity, Filter> = {
   sort_direction: string | null;
   filter: string | null;
 };
-export class SearchResult<E extends Entity, Filter = string> {
+export class SearchResult<E extends Entity = Entity, Filter = string> {
   readonly items: E[];
   readonly total: number;
   readonly current_page: number;
