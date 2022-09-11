090e0f2c49e3a87eb1b090343f4c3accf0db91ea
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2022-09-11 11:15:11 -0300

    Adiciona testes para caso de uso de atualização de categoria

--- src/category/application/use-cases/__tests__/update-category.use-case.spec.ts
+++ src/category/application/use-cases/__tests__/update-category.use-case.spec.ts
@@ -15,14 +15,30 @@ describe("UpdateCategoryUseCase unit tests", () => {
 
   it("should update a category", async () => {
     const category = new Category({ name: "Movie" }, new UniqueEntityId("5d91e43a-cdcc-43df-afb0-31563fa00751"));
-
     repository.items = [category];
 
     const findByIdSpy = jest.spyOn(repository, "findById");
     const updateSpy = jest.spyOn(repository, "update");
-    const output = await useCase.execute({ id: category.id, name: "Series" });
 
-    expect(output.name).toBe("Series");
+    let output = await useCase.execute({ id: category.id, name: "Series" });
+    expect(output).toStrictEqual({
+      name: "Series",
+      created_at: category.created_at,
+      description: null,
+      id: category.id,
+      is_active: category.is_active,
+    });
+    expect(findByIdSpy).toBeCalled();
+    expect(updateSpy).toBeCalled();
+
+    output = await useCase.execute({ id: category.id, name: "Movies", description: "Some description" });
+    expect(output).toStrictEqual({
+      name: "Movies",
+      created_at: category.created_at,
+      description: "Some description",
+      id: category.id,
+      is_active: category.is_active,
+    });
     expect(findByIdSpy).toBeCalled();
     expect(updateSpy).toBeCalled();
   });
