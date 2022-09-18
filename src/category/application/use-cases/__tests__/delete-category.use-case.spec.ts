07a50b29e670f62a4e248e2cc5ddd152538b1196
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2022-09-18 11:49:08 -0300

    Adiciona teste para exclusão de categoria

---
+++ src/category/application/use-cases/__tests__/delete-category.use-case.spec.ts
@@ -0,0 +1,35 @@
+import { Category } from "../../../domain/entities/category";
+import { NotFoundError } from "../../../../@seedwork/errors/not-found.error";
+import { CategoryInMemoryRepository } from "../../../infra/repository/category-in-memory.repository";
+import GetCategoryUseCase from "../get-category.use-case";
+import DeleteCategoryUseCase from "../delete-category.use-case";
+
+describe("DeleteCategoryUseCase unit tests", () => {
+  let useCase: DeleteCategoryUseCase;
+  let repository: CategoryInMemoryRepository;
+
+  beforeEach(() => {
+    repository = new CategoryInMemoryRepository();
+    useCase = new DeleteCategoryUseCase(repository);
+  });
+
+  it("should throws an error when category was not found", async () => {
+    await expect(() =>
+      useCase.execute({
+        id: "FAKE_ID",
+      })
+    ).rejects.toThrow(NotFoundError);
+  });
+
+  it("should get a category", async () => {
+    const category = new Category({ name: "Movie" });
+
+    repository.items = [category];
+
+    const deleteSpy = jest.spyOn(repository, "delete");
+
+    await useCase.execute({ id: category.id });
+
+    expect(deleteSpy).toBeCalled();
+  });
+});
